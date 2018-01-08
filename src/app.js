a7efda7280fd5080d8e55f242859635b9c700db7
Hsin Yen Chung <HsinYen.Chung@wu.com>
2018-01-07 23:59:47 -0800

    react webpack scss setup

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import './assets/scss/app.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// main app
+import App from './assets/js/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,102 @@
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractPlugin = new ExtractTextPlugin({
+  filename: './assets/css/app.css'
+});
+
+const config = {
+
+	context: path.resolve(__dirname, 'src'),
+
+	entry: {
+    	// removing 'src' directory from entry point, since 'context' is taking care of that
+		app: './app.js'
+	},
+
+  	output: {
+    	path: path.resolve(__dirname, 'dist'),
+    	filename: './assets/js/[name].bundle.js'
+  	},
+
+  	module: {
+	  rules: [
+	    //babel-loader
+	    {
+			test: /\.(js|jsx)$/,
+			exclude: /node_modules/,
+			include: /src/,
+			loader: 'babel-loader'
+		},
+
+	     //html-loader
+	    { 
+	    	test: /\.html$/, 
+	    	use: ['html-loader']
+	    },
+
+	    {
+		  test: /\.scss$/,
+		  include: [path.resolve(__dirname, "src/assets/scss")],
+		  use: extractPlugin.extract({
+		  	publicPath: '../../',
+		    use: [
+		    	{
+			        loader: 'css-loader',
+			        options: {
+			        }
+			    },
+			    {
+			        loader: 'sass-loader',
+			        options: {
+			          sourceMap: true
+			        }
+			    }
+    		],
+		    fallback: 'style-loader'
+		  })
+		}, 
+
+		{
+		  test: /\.(jpg|png|gif|svg)$/,
+		  use: [
+		    {
+		      loader: 'file-loader',
+		      options: {
+		        name: '[name].[ext]',
+		        outputPath: './assets/media/'
+		      }
+		    }
+		  ]
+		},
+
+		{
+		  test: /\.(woff|woff2|eot|ttf|otf)$/,
+		  use: ['file-loader']
+		}
+
+	  ]
+	},
+
+	plugins: [
+  		new CleanWebpackPlugin(['dist']),
+  		new HtmlWebpackPlugin({template: 'index.html'}),
+  		extractPlugin
+	],
+
+  	devServer: {
+	  contentBase: path.resolve(__dirname, "./dist/assets/media"),
+	  compress: true,
+	  port: 12000,
+	  stats: 'errors-only',
+	  open: true
+	},
+	//source-map use for production debug
+	devtool: 'inline-source-map'
+	
+
+}
+
+module.exports = config;
